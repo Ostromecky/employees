4ba66285b7f31f99db8c55fe10e9956d635d78a2
Paweł Ostromecki <pawel.ostromecki@angrynerds.pl>
2020-05-10 12:15:26 +0200

    Add docker config depending on environmental variables

--- services/src/config/config.service.ts
+++ services/src/config/config.service.ts
@@ -6,11 +6,11 @@ export class ConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres' as 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'employees',
+      host: process.env.DB_HOST || 'postgres',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_SCHEMA || 'employees',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     };
