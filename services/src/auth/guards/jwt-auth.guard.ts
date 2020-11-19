f2b39c1bfb755c29d763418ef06e0c7263732d5f
Paweł Ostromecki <pawel.ostromecki@sendit.pl>
2020-11-19 16:59:51 +0100

    Fix

--- services/src/auth/guards/jwt-auth.guard.ts
+++ services/src/auth/guards/jwt-auth.guard.ts
@@ -7,7 +7,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    if (JSON.parse(process.env.AUTH_ENABLED) === false) return true;
+    if (process.env.AUTH_ENABLED == 'false') return true;
     return super.canActivate(context);
   }
 }
